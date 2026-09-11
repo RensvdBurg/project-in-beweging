3f54276b78ce1f3d9f97896cfeafcf41d3965001
RensvdBurg <rensvdburg050@hotmail.com>
2026-09-11 13:33:37 +0200

    Icons toegevoegd

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <title>Mijn project</title>
 
-    <link rel="stylesheet" href="css/output.css">
-</head>
 
 <body>
+    <?php require __DIR__ . '/components/header.php'; ?>
     <h1 class="text-4xl font-bold text-blue-500">
         Hallo wereld!
     </h1>
 </body>
-</html>
